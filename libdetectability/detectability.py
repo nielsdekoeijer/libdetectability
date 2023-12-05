f6240a9615d7c6756ac053fcc2435431c6581e92
nielsdekoeijer <62387960+nielsdekoeijer@users.noreply.github.com>
2023-12-05 11:15:38 +0100

    Update detectability.py

--- libdetectability/detectability.py
+++ libdetectability/detectability.py
@@ -75,7 +75,7 @@ class Detectability:
         return np.sqrt(G.sum(axis=0))
 
 class DetectabilityLoss(tc.nn.Module):
-    def __init__(self, frame_size=2048, sampling_rate=48000, taps=32, dbspl=94.0, spl=1.0, relax_threshold=False, norm = "backward"):
+    def __init__(self, frame_size=2048, sampling_rate=48000, taps=32, dbspl=94.0, spl=1.0, relax_threshold=True, norm = "backward", reduction="meanlog"):
         super(DetectabilityLoss, self).__init__()
         self.detectability = Detectability(frame_size=frame_size, sampling_rate=sampling_rate, taps=taps, dbspl=dbspl, \
                 spl=spl, relax_threshold=relax_threshold, norm=norm)
@@ -86,6 +86,7 @@ class DetectabilityLoss(tc.nn.Module):
         self.leff = self.detectability.leff
         self.norm = self.detectability.norm
         self.G = tc.from_numpy(self.detectability.h) * tc.from_numpy(self.detectability.g).unsqueeze(0)
+        self.reduction = reduction
 
     def _spectrum(self, a):
         return tc.pow(tc.abs(tc.fft.rfft(a, axis=1, norm=self.norm)), 2.0)
@@ -114,4 +115,8 @@ class DetectabilityLoss(tc.nn.Module):
 
     def forward(self, reference, test):
         batches = self.frame(reference, test)
-        return batches.mean()
+        if self.reduction == "mean":
+            return batches.mean()
+        if self.reduction == "meanlog":
+            batches = tc.log(batches)
+            return batches.mean()
