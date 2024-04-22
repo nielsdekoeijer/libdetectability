be6e48a4b24ff251ad57761b3f119195c975cd8b
nielsdekoeijer <62387960+nielsdekoeijer@users.noreply.github.com>
2024-04-22 12:45:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="libdetectability",
-    version="0.4.1",
+    version="0.4.2",
     packages=find_packages(),
     install_requires=["pytest", "numpy", "scipy", "torch"],
     test_suite="test",
