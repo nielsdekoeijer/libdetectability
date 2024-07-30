ca5fcb06f76d3c5c48174912733cb94be719747f
Your Name <you@example.com>
2024-07-30 17:00:45 +0200

    reduction=None flag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="libdetectability",
-    version="0.5.2",
+    version="0.5.0",
     packages=find_packages(),
     install_requires=["pytest", "numpy", "scipy", "torch","libsegmenter"],
     test_suite="test",
