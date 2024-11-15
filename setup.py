499a0bfa450346b5b6428555afe8936e08d46253
Your Name <you@example.com>
2024-11-15 10:26:39 +0100

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="libdetectability",
-    version="0.6.0",
+    version="0.6.1",
     packages=find_packages(),
     install_requires=["pytest", "numpy", "scipy", "torch", "libsegmenter"],
     test_suite="test",
