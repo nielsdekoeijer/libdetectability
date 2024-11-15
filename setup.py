2ca60b1af66234eaad35cacc1f2a06b8a8fd67b8
Your Name <you@example.com>
2024-11-15 10:29:20 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="libdetectability",
-    version="0.6.1",
+    version="0.7.0",
     packages=find_packages(),
     install_requires=["pytest", "numpy", "scipy", "torch", "libsegmenter"],
     test_suite="test",
