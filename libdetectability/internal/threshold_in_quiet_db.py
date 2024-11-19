ef94ec53e1e973abf298300013f7c1cffa69ed10
nielsdekoeijer <62387960+nielsdekoeijer@users.noreply.github.com>
2024-11-19 11:13:22 +0100

    Update threshold_in_quiet_db.py

--- libdetectability/internal/threshold_in_quiet_db.py
+++ libdetectability/internal/threshold_in_quiet_db.py
@@ -2,11 +2,12 @@ import numpy as np
 
 
 def threshold_in_quiet_db(freq, regularized=False):
-    value = (
-        3.64 * np.power(freq / 1000.0, -0.8)
-        - 6.5 * np.exp(-0.6 * np.power(freq / 1000.0 - 3.3, 2))
-        + 10e-4 * np.power(freq / 1000.0, 4)
-    )
+    with np.errstate(divide='ignore'):
+        value = (
+            3.64 * np.power(freq / 1000.0, -0.8)
+            - 6.5 * np.exp(-0.6 * np.power(freq / 1000.0 - 3.3, 2))
+            + 10e-4 * np.power(freq / 1000.0, 4)
+        )
 
     if regularized:
         return np.maximum(value, 30)
