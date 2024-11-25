653abd6859728192db9ca4d803289a230c915041
Your Name <you@example.com>
2024-11-25 17:05:29 +0100

    update

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="libdetectability",
-    version="0.8.0",
-    packages=find_packages(),
-    install_requires=["pytest", "numpy", "scipy", "torch", "libsegmenter"],
-    test_suite="test",
-)
